fab945b1b472a11e63de58ce1d6fe54992ac16ca
Leonardo Motta de Oliveira <leonardomottadev@gmail.com>
2026-08-17 23:26:23 -0300

    feat: solve add two numbers

--- 0002-add-two-numbers/ListNode.java
+++ 0002-add-two-numbers/ListNode.java
@@ -1,7 +1,35 @@
 class ListNode {
     int val;
     ListNode next;
-    ListNode() {}
-    ListNode(int val) { this.val = val; }
-    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+
+    ListNode() {
+
+    }
+
+    ListNode(int val) { 
+        this.val = val; 
+    }
+
+    ListNode(int val, ListNode next) { 
+        this.val = val; this.next = next; 
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder resultado = new StringBuilder("[");
+        ListNode node = this;
+
+        while (node != null) {
+            resultado.append(node.val);
+
+            if (node.next != null) {
+                resultado.append(",");
+            }
+
+            node = node.next;
+        }
+
+        resultado.append("]");
+        return resultado.toString();
+    }
 }
--- 0002-add-two-numbers/Solution.java
+++ 0002-add-two-numbers/Solution.java
@@ -29,13 +29,97 @@ Constraints:
 
 */
 
+import java.util.ArrayList;
+import java.util.List;
 
 class Solution {
     public static void main(String args[]) {
-        System.out.println("Teste");
+        // Exemplo 1
+        // l1 = [2,4,3]
+        ListNode l1Exemplo1 = new ListNode(2);
+        l1Exemplo1.next = new ListNode(4);
+        l1Exemplo1.next.next = new ListNode(3);
+
+        // l2 = [5,6,4]
+        ListNode l2Exemplo1 = new ListNode(5);
+        l2Exemplo1.next = new ListNode(6);
+        l2Exemplo1.next.next = new ListNode(4);
+
+        System.out.println(addTwoNumbers(l1Exemplo1, l2Exemplo1));
+
+
+        // Exemplo 2
+        // l1 = [0]
+        ListNode l1Exemplo2 = new ListNode(0);
+
+        // l2 = [0]
+        ListNode l2Exemplo2 = new ListNode(0);
+
+        System.out.println(addTwoNumbers(l1Exemplo2, l2Exemplo2));
+
+
+        // Exemplo 3
+        // l1 = [9,9,9,9,9,9,9]
+        ListNode l1Exemplo3 = new ListNode(9);
+        l1Exemplo3.next = new ListNode(9);
+        l1Exemplo3.next.next = new ListNode(9);
+        l1Exemplo3.next.next.next = new ListNode(9);
+        l1Exemplo3.next.next.next.next = new ListNode(9);
+        l1Exemplo3.next.next.next.next.next = new ListNode(9);
+        l1Exemplo3.next.next.next.next.next.next = new ListNode(9);
+
+        // l2 = [9,9,9,9]
+        ListNode l2Exemplo3 = new ListNode(9);
+        l2Exemplo3.next = new ListNode(9);
+        l2Exemplo3.next.next = new ListNode(9);
+        l2Exemplo3.next.next.next = new ListNode(9);
+
+        System.out.println(addTwoNumbers(l1Exemplo3, l2Exemplo3));
     }
 
     public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        return new ListNode();
+        
+        int addNext = 0;
+        int result = l1.val + l2.val + addNext;
+        if(result > 9) {
+            addNext = result / 10;
+            result = result % 10;
+        } else {
+            addNext = 0;
+        }
+
+        ListNode response = new ListNode(result);
+        ListNode initialResponseNode = response;
+
+        while(true) {
+            if(l1.next != null && l2.next != null) {
+                l1 = l1.next;
+                l2 = l2.next;
+            }
+            else if(l1.next != null && l2.next == null) {
+                l1 = l1.next;
+                l2 = new ListNode(0);
+            } 
+            else if(l1.next == null && l2.next != null) {
+                l1 = new ListNode(0);
+                l2 = l2.next;
+            }
+            else {
+                if(addNext != 0) {
+                    response.next = new ListNode(addNext);
+                }
+                break;
+            }
+            result = l1.val + l2.val + addNext;
+            if(result > 9) {
+                addNext = result / 10;
+                result = result % 10;
+            } else {
+                addNext = 0;
+            }
+            response.next = new ListNode(result);
+            response = response.next;
+        }
+        return initialResponseNode;
     }
 }
